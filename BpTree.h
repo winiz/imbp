769588a94a95ef3b7847cf62f7225918a520f473
Xinran Zhang <winizhang1995@gmail.com>
2016-07-12 22:21:57 +0000

    first commit

---
+++ BpTree.h
@@ -0,0 +1,8 @@
+/* class declarations goes here */
+/* §  BpTree(int) – this constructor creates an empty tree, the parameter sets the number of keys that can be stored in each node (leaf and interior)
+///§  insert(int, string) – inserts the key (int) and value (string) into the tree
+///§  remove(int) – searches the tree for the key (int), if found removes the key and associated value from the tree
+///§  find(int) – searches the tree for the key (int), if found returns the matching value
+///§  printKeys() – prints the keys in each node with nodes of the same level appearing on the same line
+///§  printValues() – prints the values in key (not value) order
+ /
