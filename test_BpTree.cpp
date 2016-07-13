2e5f969b929ed917c457dcb52b6119fd5a9ea591
Xinran Zhang <winizhang1995@gmail.com>
2016-07-13 03:01:16 +0000

    ...

---
+++ test_BpTree.cpp
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include "list.h"
+#include <stdbool.h>
+#include <string.h>
+#include <assert.h>
+
+/*
+ * Main()
+ List_printNodes (head);
+ */
+int main(int argc, char** argv)
+{
+    return 0;
+    printf("Starting tests...\n");
+	//struct nodeStruct *head = NULL;
+}
