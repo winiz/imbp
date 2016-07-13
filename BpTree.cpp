cece8a0cffea7c7b6e9fdfa6b2888286855af030
winiz <winizhang1995@gmail.com>
2016-07-13 14:52:05 +0000

    implementing insert

--- BpTree.cpp
+++ BpTree.cpp
@@ -36,6 +36,34 @@ BTreeNode* BTreeNode::find(int k){
     return C[i]->find(k);
 }
 
+void BTreeNode::insertFull(int k){
+    // Allocate memory for new root
+    BTreeNode *nr = new BTreeNode(t, false);
+ 
+    // Make old root as child of new root
+    nr->C[0] = root;
+ 
+    // Split the old root and move 1 key to the new root
+    nr->splitChild(0, root);
+ 
+    // New root has two children now.  Decide which of the
+    // two children is going to have new key
+    int i = 0;
+    if (nr->keys[0] < k)
+    i++;
+    nr->C[i]->insertNonFull(k);
+ 
+    // Change root
+    root = nr;
+}
+
+void BTreeNode::insertnonFull(int k){
+   
+}
+    
+void BTreeNode::splitChild(int i, BTreeNode *y){
+    
+}
 
 // Constructor for BpTree 
 BpTree::BpTree(int _n){  
@@ -48,4 +76,25 @@ BpTree::BpTree(int _n){
 // Function to search a key in this tree
 BTreeNode* BpTree::find(int k){  
     return (root == NULL)? NULL : root->find(k); 
+}
+
+void BpTree::insert(int k){
+    // If tree is empty
+    if (root == NULL)
+    {
+        // Allocate memory for root
+        root = new BTreeNode(t, true);
+        root->keys[0] = k;  // Insert key
+        root->ck = 1;  // Update number of keys in root
+    }
+    else // If tree is not empty
+    {
+        // If root is full, then tree grows in height
+        if (root->ck == n)
+        {
+            
+        }
+        else  // If root is not full, call insertNonFull for root
+            root->insertNonFull(k);
+    }
 }
--- BpTree.h
+++ BpTree.h
@@ -9,11 +9,22 @@ class BTreeNode
     int n;      // Maximum number of search keys 
     int *keys;  // An array of keys
     BTreeNode **C; // An array of child pointers
-    int ck;     // Current number of keys
+    int ck;     // Current number of search keys
     bool leaf; // Is true when node is leaf. Otherwise false
+    
+    void insertNonFull(int k);
+    
+    void splitChild(int i, BTreeNode *y);
+    
 public:
     BTreeNode(int _n, bool _leaf);   // Constructor
- 
+    
+    // A utility function to insert a new key in the subtree rooted with
+    // this node. The assumption is, the node must be non-full when this
+    // function is called
+    // A utility function to split the child y of this node. i is index of y in
+    // child array C[].  The Child y must be full when this function is called
+    
     // A function to search a key in subtree rooted with this node.    
     // returns NULL if k is not present.
     BTreeNode *find(int k); 
@@ -34,6 +45,8 @@ public:
     
     // function to search a key in this tree
     BTreeNode* find(int k);
+    
+    void insert(int k);
 };
 
 #endif //BPTREE_H
